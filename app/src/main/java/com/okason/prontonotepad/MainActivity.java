83fa29f3e5d2783ad976a733a4834dbb4ecda56a
Val Okafor <vokafor@processingpoint.com>
2016-12-23 07:07:35 -0800

    Implement SignIn with Firebase UI

--- app/src/main/java/com/okason/prontonotepad/MainActivity.java
+++ app/src/main/java/com/okason/prontonotepad/MainActivity.java
@@ -1,31 +1,88 @@
 package com.okason.prontonotepad;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.MainThread;
+import android.support.annotation.StringRes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.okason.prontonotepad.auth.AuthUiActivity;
+import com.okason.prontonotepad.model.Note;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+    private  FloatingActionButton fab;
+
+    @BindView(android.R.id.content)
+    View mRootView;
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    private static final String GOOGLE_TOS_URL = "https://www.google.com/policies/terms/";
+    public static final String DRIVE_FILE = "https://www.googleapis.com/auth/drive.file";
+    private static final int RC_SIGN_IN = 100;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+
+        if (mFirebaseUser == null){
+            //Not signed in, launch the Sign In Activity
+            startActivity(new Intent(this, AuthUiActivity.class));
+            finish();
+            return;
+        }
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                addInitialDataToFirebase();
             }
         });
+    }
+
+
+
+    private void addInitialDataToFirebase() {
+        Note note1 = new Note();
+        note1.setNoteId("ABC123");
+        note1.setTitle("DisneyLand Trip");
+        note1.setContent("We went to Disneyland today and the kids had lots of fun!");
+        Calendar calendar1 = GregorianCalendar.getInstance();
+        note1.setDateModified(calendar1.getTimeInMillis());
+        note1.setCategoryId("ADC432");
+
+        //mDatabase.child("notes").push().setValue(note1);
+        DatabaseReference userSpecificRef =  mDatabase.child("users/" + mFirebaseUser.getUid());
+        userSpecificRef.push().setValue(note1);
+
     }
 
     @Override
@@ -49,4 +106,13 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+    @MainThread
+    private void showSnackbar(@StringRes int errorMessageRes) {
+        Snackbar.make(mRootView, errorMessageRes, Snackbar.LENGTH_LONG).show();
+    }
+
+
 }
---
+++ app/src/main/java/com/okason/prontonotepad/model/Note.java
@@ -0,0 +1,90 @@
+package com.okason.prontonotepad.model;
+
+/**
+ * Created by vokafor on 12/20/2016.
+ */
+
+public class Note {
+
+    private String noteId;
+    private String title;
+    private String content;
+    private int color;
+    private long dateCreated;
+    private long dateModified;
+    private String audioPath;
+    private String imagePath;
+    private String categoryId;
+
+    public String getNoteId() {
+        return noteId;
+    }
+
+    public void setNoteId(String noteId) {
+        this.noteId = noteId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public long getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(long dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public long getDateModified() {
+        return dateModified;
+    }
+
+    public void setDateModified(long dateModified) {
+        this.dateModified = dateModified;
+    }
+
+    public String getAudioPath() {
+        return audioPath;
+    }
+
+    public void setAudioPath(String audioPath) {
+        this.audioPath = audioPath;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+}
