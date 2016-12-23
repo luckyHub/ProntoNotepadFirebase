e423f50ee50b10d82587ce9eb2aa66d020f6c8f7
Val Okafor <vokafor@processingpoint.com>
2016-12-23 15:47:36 -0800

    Add Navigation Drawer

--- app/src/main/java/com/okason/prontonotepad/MainActivity.java
+++ app/src/main/java/com/okason/prontonotepad/MainActivity.java
@@ -1,23 +1,43 @@
 package com.okason.prontonotepad;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.MainThread;
+import android.support.annotation.NonNull;
 import android.support.annotation.StringRes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
+import com.mikepenz.materialdrawer.model.interfaces.Nameable;
+import com.mikepenz.materialdrawer.util.KeyboardUtil;
 import com.okason.prontonotepad.auth.AuthUiActivity;
 import com.okason.prontonotepad.model.Note;
+import com.okason.prontonotepad.util.Constants;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -29,9 +49,23 @@ public class MainActivity extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
     private  FloatingActionButton fab;
+    private AccountHeader mHeader = null;
+    private Drawer mDrawer = null;
+    private Activity mActivity;
 
-    @BindView(android.R.id.content)
-    View mRootView;
+
+
+    @BindView(android.R.id.content)  View mRootView;
+    @BindView(R.id.toolbar) Toolbar toolbar;
+
+    public static final String ANONYMOUS = "anonymous";
+    public static final String ANONYMOUS_PHOTO_URL = "https://dl.dropboxusercontent.com/u/15447938/notepadapp/anon_user_48dp.png";
+    public static final String ANONYMOUS_EMAIL = "anonymous@noemail.com";
+    private static final String LOG_TAG = "MainActivity";
+    private String mUsername;
+    private String mPhotoUrl;
+    private String mEmailAddress;
+
 
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
@@ -47,6 +81,9 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mActivity = this;
+
+
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
         mFirebaseAuth = FirebaseAuth.getInstance();
@@ -57,7 +94,15 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(this, AuthUiActivity.class));
             finish();
             return;
+        }else {
+            mUsername = mFirebaseUser.getDisplayName();
+            mPhotoUrl = mFirebaseUser.getPhotoUrl().toString();
+            mEmailAddress = mFirebaseUser.getEmail();
         }
+
+        setupNavigationDrawer(savedInstanceState);
+
+
 
         fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -68,6 +113,147 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void setupNavigationDrawer(Bundle savedInstanceState) {
+        mUsername = TextUtils.isEmpty(mUsername) ? ANONYMOUS : mUsername;
+        mEmailAddress = TextUtils.isEmpty(mEmailAddress) ? ANONYMOUS_EMAIL : mEmailAddress;
+        mPhotoUrl = TextUtils.isEmpty(mPhotoUrl) ? ANONYMOUS_PHOTO_URL : mPhotoUrl;
+
+        IProfile profile = new ProfileDrawerItem()
+                .withName(mUsername)
+                .withEmail(mEmailAddress)
+                .withIcon(mPhotoUrl)
+                .withIdentifier(102);
+
+        mHeader = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.header)
+                .addProfiles(profile)
+                .build();
+        mDrawer = new DrawerBuilder()
+                .withAccountHeader(mHeader)
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withActionBarDrawerToggle(true)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName("Notes").withIcon(GoogleMaterial.Icon.gmd_view_list).withIdentifier(Constants.NOTES),
+                        new PrimaryDrawerItem().withName("Tags").withIcon(GoogleMaterial.Icon.gmd_folder).withIdentifier(Constants.TAGS),
+                        new PrimaryDrawerItem().withName("Settings").withIcon(GoogleMaterial.Icon.gmd_settings).withIdentifier(Constants.SETTINGS),
+                        new PrimaryDrawerItem().withName("Logout").withIcon(GoogleMaterial.Icon.gmd_lock).withIdentifier(Constants.LOGOUT)
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        if (drawerItem != null && drawerItem instanceof Nameable){
+                            String name = ((Nameable) drawerItem).getName().getText(mActivity);
+                            toolbar.setTitle(name);
+                        }
+
+                        if (drawerItem != null){
+                            //handle on navigation drawer item
+                            onTouchDrawer((int) drawerItem.getIdentifier());
+                        }
+                        return false;
+                    }
+                })
+                .withOnDrawerListener(new Drawer.OnDrawerListener() {
+                    @Override
+                    public void onDrawerOpened(View drawerView) {
+                        KeyboardUtil.hideKeyboard(MainActivity.this);
+                    }
+
+                    @Override
+                    public void onDrawerClosed(View drawerView) {
+
+                    }
+
+                    @Override
+                    public void onDrawerSlide(View drawerView, float slideOffset) {
+
+                    }
+                })
+                .withFireOnInitialOnClick(true)
+                .withSavedInstance(savedInstanceState)
+                .build();
+
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mDrawer.addStickyFooterItem(new PrimaryDrawerItem().withName("Delete Account!").withIcon(GoogleMaterial.Icon.gmd_delete).withIdentifier(Constants.DELETE));
+    }
+
+    private void onTouchDrawer(int position) {
+        switch (position){
+            case Constants.NOTES:
+                //Do Nothing, we are already on Notes
+                break;
+            case Constants.TAGS:
+                //Go to Tags
+                break;
+            case Constants.SETTINGS:
+                //Go to Settings
+                break;
+            case Constants.LOGOUT:
+                logout();
+                break;
+            case Constants.DELETE:
+                deleteAccountClicked();
+                break;
+        }
+
+    }
+
+    private void logout() {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            startActivity(new Intent(mActivity, MainActivity.class));
+                            finish();
+                        } else {
+                            showSnackbar(R.string.sign_out_failed);
+                        }
+                    }
+                });
+
+    }
+
+    public void deleteAccountClicked() {
+
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setMessage("Are you sure you want to delete this account?")
+                .setPositiveButton("Yes, nuke it!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        deleteAccount();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .create();
+
+        dialog.show();
+    }
+
+    private void deleteAccount() {
+        AuthUI.getInstance()
+                .delete(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            startActivity(new Intent(mActivity, MainActivity.class));
+                            finish();
+                        } else {
+                            showSnackbar(R.string.delete_account_failed);
+                        }
+                    }
+                });
+    }
 
 
     private void addInitialDataToFirebase() {
@@ -113,6 +299,8 @@ public class MainActivity extends AppCompatActivity {
     private void showSnackbar(@StringRes int errorMessageRes) {
         Snackbar.make(mRootView, errorMessageRes, Snackbar.LENGTH_LONG).show();
     }
+
+
 
 
 }
---
+++ app/src/main/java/com/okason/prontonotepad/util/Constants.java
@@ -0,0 +1,15 @@
+package com.okason.prontonotepad.util;
+
+/**
+ * Created by vokafor on 12/23/2016.
+ */
+
+public class Constants {
+    public final static int NOTES = 1;
+    public final static int TAGS = 2;
+    public final static int SETTINGS = 3;
+    public final static int LOGOUT = 4;
+    public final static int DELETE = 5;
+
+
+}
