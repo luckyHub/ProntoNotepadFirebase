3b3f0b50a26e8cab3b0c685de71024a409dcfbac
luckyHub <tonypeacock47@gmail.com>
2017-05-18 13:42:56 -0400

    Commit of Navigation Drawer

--- app/src/main/java/com/okason/prontonotepad/MainActivity.java
+++ app/src/main/java/com/okason/prontonotepad/MainActivity.java
@@ -1,15 +1,51 @@
 package com.okason.prontonotepad;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
+import com.mikepenz.materialdrawer.model.interfaces.Nameable;
+import com.mikepenz.materialdrawer.util.KeyboardUtil;
+import com.okason.prontonotepad.auth.AuthUiActivity;
+import com.okason.prontonotepad.util.Constants;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    private String mUsername;
+    private String mEmailAddress;
+    private String mPhotoUrl;
+    public static final String ANONYMOUS = "anonymous";
+    public static final String ANONYMOUS_PHOTO_URL = "https://dl.dropboxusercontent.com/u/15447938/notepadapp/anon_user_48dp.png";
+    public static final String ANONYMOUS_EMAIL = "anonymous@noemail.com";
 
+    private AccountHeader mHeader = null;
+    private Drawer mDrawer = null;
+    private Activity mActivity;
 
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
 
     @Override
@@ -19,8 +55,139 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mActivity = MainActivity.this;
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        //check if user signed in
+        if (mFirebaseUser == null) {
+            startActivity(new Intent(MainActivity.this, AuthUiActivity.class));
+            finish();
+            return;
+        } else {
+            //user signed in
+            mUsername = mFirebaseUser.getDisplayName();
+            mEmailAddress = mFirebaseUser.getEmail();
+            mPhotoUrl = mFirebaseUser.getPhotoUrl().toString();
+        }
+
+        setUpNavigationDrawer(savedInstanceState);
+
+
+        FloatingActionButton floatingBtn = (FloatingActionButton) findViewById(R.id.fab);
+        floatingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "replace action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
 
     }
+
+    private void setUpNavigationDrawer(Bundle savedInstanceState) {
+        mUsername = TextUtils.isEmpty(mUsername) ? ANONYMOUS : mUsername;
+        mPhotoUrl = TextUtils.isEmpty(mPhotoUrl) ? ANONYMOUS_PHOTO_URL : mPhotoUrl;
+        mEmailAddress = TextUtils.isEmpty(mEmailAddress) ? ANONYMOUS_EMAIL : mEmailAddress;
+
+        IProfile profile = new ProfileDrawerItem()
+                .withName(mUsername)
+                .withEmail("someemail@gymmail.com")
+                .withIcon(mPhotoUrl)
+                .withIdentifier(102);
+
+        mHeader = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.header_2)
+                .addProfiles(profile)
+                .build();
+
+        mDrawer = new DrawerBuilder()
+                .withAccountHeader(mHeader)
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withActionBarDrawerToggle(true)
+                .addDrawerItems(new PrimaryDrawerItem()
+                                .withName("Notes")
+                                .withIcon(GoogleMaterial.Icon.gmd_view_list)
+                                .withIdentifier(Constants.NOTES),
+                        new PrimaryDrawerItem().withName("Categories").withIcon(GoogleMaterial.Icon.gmd_folder).withIdentifier(Constants.CATEGORIES),
+                        new PrimaryDrawerItem().withName("Settings").withIcon(GoogleMaterial.Icon.gmd_settings).withIdentifier(Constants.SETTINGS),
+                        new PrimaryDrawerItem().withName("Logout").withIcon(GoogleMaterial.Icon.gmd_lock)
+                                .withIdentifier(Constants.LOGOUT)
+                )
+                //add listener for item clicks
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        //set title of toolbar
+                        if (drawerItem != null && drawerItem instanceof Nameable) {
+                            String name = ((Nameable) drawerItem).getName().getText(mActivity);
+                            toolbar.setTitle(name);
+                        }
+                        if (drawerItem != null){
+                            onTouchDrawer((int) drawerItem.getIdentifier());
+                        }
+                        return false;
+                    }
+                })
+                //drawer listener
+                .withOnDrawerListener(new Drawer.OnDrawerListener() {
+                    @Override
+                    public void onDrawerOpened(View drawerView) {
+                        KeyboardUtil.hideKeyboard(MainActivity.this);
+                    }
+
+                    @Override
+                    public void onDrawerClosed(View drawerView) {
+
+
+                    }
+
+                    @Override
+                    public void onDrawerSlide(View drawerView, float slideOffset) {
+
+                    }
+                })
+                .withFireOnInitialOnClick(true)
+                .withSavedInstance(savedInstanceState)
+                .build();
+        //add footer , outside of DrawerBuilder();
+        mDrawer.addStickyFooterItem(new PrimaryDrawerItem()
+                .withName("Delete Account!")
+                .withIcon(GoogleMaterial.Icon.gmd_delete)
+                .withIdentifier(Constants.DELETE));
+
+    }
+
+    private void onTouchDrawer(int position) {
+        switch (position){
+            case Constants.NOTES:
+                //Do Nothing, we are already on Notes
+                break;
+            case Constants.CATEGORIES:
+                ///startActivity(new Intent(NoteListActivity.this, CategoryActivity.class));
+                break;
+            case Constants.SETTINGS:
+                //Go to Settings
+                ///startActivity(new Intent(NoteListActivity.this, SettingsActivity.class));
+                break;
+            case Constants.LOGOUT:
+                ///logout();
+                break;
+            case Constants.DELETE:
+                ///deleteAccountClicked();
+                break;
+        }
+
+    }
+
+
+
+
+
 
 
 
